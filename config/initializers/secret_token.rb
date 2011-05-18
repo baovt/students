4cac458ad407aacd37e5d755b898a3c7c9e10750
Bao Vuong <bao@mars.(none)>
2011-05-18 15:30:42 +0700

    students scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Students::Application.config.secret_token = '5db8a2d94bafc970a6aaf9ad8c299758f95c6da9e3f9dfcbff866baf2936fc35d44f17dc73912eec02c1b78a84f04e4d73e4d878fc0a82ef6d7f4b54b26fab8a'
